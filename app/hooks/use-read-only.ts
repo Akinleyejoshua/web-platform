6fe228a1c3158db9a89786d89a5bf11bf03a83da
Akinleyejoshua <akinleyejoshua.dev@gmail.com>
2026-06-13 19:52:11 +0100

    fixes

--- app/hooks/use-read-only.ts
+++ app/hooks/use-read-only.ts
@@ -4,15 +4,19 @@ import { useState, useEffect } from 'react';
 
 export function useReadOnly() {
     const [isReadOnly, setIsReadOnly] = useState(false);
+    if (typeof window === 'undefined') {
+        return false; // Return false during server-side rendering
+    } else {
+        useEffect(() => {
+            const checkReadOnly = () => {
+                if (typeof document === 'undefined') return false;
+                const match = document.cookie.match(/admin_read_only=true/);
+                return !!match;
+            };
+            setIsReadOnly(checkReadOnly());
+        }, [location.pathname]); // Re-run when the pathname changes
+    }
 
-    useEffect(() => {
-        const checkReadOnly = () => {
-            if (typeof document === 'undefined') return false;
-            const match = document.cookie.match(/admin_read_only=true/);
-            return !!match;
-        };
-        setIsReadOnly(checkReadOnly());
-    }, [window.location.pathname]); // Re-run when the pathname changes
 
     return isReadOnly;
 }
